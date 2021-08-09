fa69a582dc8a79525c016d5765f8d9621d67838b
MageshSundarG <sit19cs070@sairamtap.edu.in>
2021-08-09 22:25:48 +0530

    mobile__feature__update

---
+++ main.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    var x = document.getElementsByClassName("navbar__nav__dropdown__items");
+    console.log(x);
+    if (x[0].style.display === "block") {
+        x[0].style.display = "none";
+    } else {
+        x[0].style.display = "block";
+    }
+}
